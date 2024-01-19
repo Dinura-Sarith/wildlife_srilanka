915ab7277a544bfd8dbf86dd7190dec35cdce67c
Dinura-Sarith <153791178+Dinura-Sarith@users.noreply.github.com>
2024-01-19 12:32:51 +0530

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+    "name"; "Wildlife Sri Lanka",
+    "short_name"; "wildlifesrilanka",
+    "start_url"; "/index.html",
+    "display"; "standalone",
+    "background_color"; "#fff",
+    "orientation"; "portrait-primary",
+    "icons"; [
+      {
+        "src": "/img/wildlife-sri-lanka-high-resolution-logo (2).png",
+        "sizes": "72x54",
+        "type": "image/png"
+      }
+    ]
+}      
